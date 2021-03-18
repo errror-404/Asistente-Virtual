2155fdd907083825a1048c21816fc5134a0874a5
jaberwooc <79779934+jaberwooc@users.noreply.github.com>
2021-03-18 15:49:51 -0700

    Add files via upload

--- Waifu.py
+++ Waifu.py
@@ -1,23 +1,36 @@
 import speech_recognition as sr
 import pyttsx3
-
+import time
 listener = sr.Recognizer()
 
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[2].id)
+engine.setProperty('voice',voices[0].id)
+name = "Alexa"
+for voice in voices:
+ print(voice)
 
+def talk(text): 
+ engine.say(text)
+ engine.runAndWait()
 
+def listen():
+    try:
+       with sr.Microphone() as source:
+         print("Escuchando...")
+         voice = listener.listen(source)
+         rec = listener.recognize_google(voice , language= "es-MX")
+         if name in rec:
+             print(rec)
 
+    except:
+       pass
+    return rec
 
-try:
-    with sr.Microphone() as source:
-        print("Escuchando...")
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        engine.say(rec)
-        engine.runAndWait()
-        print(rec)
-except:
-    pass
+def run():
+    rec = listen()
+
+    if "reproduce" in rec:
+        talk(rec)
+run()
